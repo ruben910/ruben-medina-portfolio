f6ba563d4a6b12e6c65d9d5a72b66a16f32bcbbf
ruben910 <rubenmedina910@gmail.com>
2026-07-22 10:57:53 -0400

    chore: clean up unused funnel files and implement minimalist dark portfolio redesign

--- revert2.py
+++
@@ -1,9 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('src="assets/og_image_premium.png" alt="Rubén Medina', 'src="assets/perfil_ruben.jpg" alt="Rubén Medina')
-
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(text)
-
-print("Reverted!")
